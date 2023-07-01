369a3b39099ef6b60b87c6ec33ef6bd01b310154
Chawischi <lipechaw@gmail.com>
2023-07-01 17:04:20 -0300

    ainda em processo

---
+++ guias/gerenciareventos.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    // Código de inicialização do carousel aqui
+    var carouselElement = document.getElementById('CarouselEventos');
+    var carousel = new bootstrap.Carousel(carouselElement);
+  };
+  
